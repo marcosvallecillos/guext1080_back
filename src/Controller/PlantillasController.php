126af759688f1ff64c2de53d3ce07c4654667dea
Alberto Villegas <alvich@floridauniversitaria.es>
2025-04-13 18:50:45 +0200

    Haciendo cambios controlador plantillas

--- src/Controller/PlantillasController.php
+++ src/Controller/PlantillasController.php
@@ -11,7 +11,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/plantillas')]
 final class PlantillasController extends AbstractController
 {
     #[Route(name: 'app_plantillas_index', methods: ['GET'])]
